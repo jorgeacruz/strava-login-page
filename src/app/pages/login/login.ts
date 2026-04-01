3e21c6368b87608f6c3062aaeacdbb63dc804e32
jorgeacruz <j.alexcruz@gmail.com>
2026-04-01 10:48:10 -0300

    Add form + fields

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { DefautLoginLayout } from '../../components/defaut-login-layout/defaut-login-layout';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
-    DefautLoginLayout
+    DefautLoginLayout,
+    ReactiveFormsModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginForm!: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+}
